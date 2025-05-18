fa8a01506f47d47e4eebaa269a709121dd641b9d
nabihakhan6t4 <khannabiha923@gmail.com>
2025-05-18 15:45:41 +0300

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login";
